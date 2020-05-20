577333bacdcf0fd1954294234715d86b4f33dcb9
Proful <butu25@gmail.com>
2020-05-20 18:57:30 +1000

    session 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react'
 
-import Hello from './Hello'
 import Training from './Training'
 
 function App() {
   return (
     <>
-      <h1>Hello Everyoone</h1>
-      <a href="www.google.com"></a>
-      <br />
-      <Hello name="Amune" age="23" />
-      <br />
-      <Training />
+        <h1>hello world</h1>
+        <Training />
     </>
   );
 }
--- src/Training.js
+++ src/Training.js
@@ -1,8 +1,34 @@
 import React from "react";
 
+import Hello from "./Hello";
+
 class Training extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            "name" : "Steve"
+        }
+    }
+
+    handleClick = () => {
+        console.log("you have clicked..");
+        // ❌this.state.name = "Patrick"
+
+        this.setState({
+            name: "Patrick"
+        })
+
+        console.log("State: " + this.state.name)//❌ Wrong vallue
+    }
+
     render () {
-        return <h1>Hello from Training Component...</h1>
+        return (
+          <div>
+            <h1>Hello from {this.state.name }</h1>
+
+            <button onClick={this.handleClick}>Change Name</button>
+          </div>
+        );
     }
 }
 
